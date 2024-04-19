e550ad7d4ddf5f8875d8eea4e3b4b3ecadcde74d
PBaloyi449 <pbaloyi449@gmail.com>
2024-04-19 11:55:58 +0200

    imported files

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // TASK: import helper functions from utils
+import { getTasks, createNewTask, patchTask, putTask, deleteTask } from './taskFunction.js';
+
 // TASK: import initialData
+import { initialData } from './initialData.js';
 
 
 /*************************************************************************************************************************************************
